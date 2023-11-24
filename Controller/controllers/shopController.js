2e171061a4d20b9d9917846c639945861c2e45bc
Aras <vahitaras@gmail.com>
2023-11-24 05:13:14 -0500

    blog page created

--- Controller/controllers/shopController.js
+++ Controller/controllers/shopController.js
@@ -362,9 +362,15 @@ exports.getInvoice = async (req, res, next) => {
 };
 
 exports.getBlogPage = async (req, res, next) => {
+  const blogtitle = "Lorem Ipsum Title";
+
+  const blogContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   return res.render("blog", {
     pagePath: "/blog",
     renderTitle: "Blog Posts",
+    blogTitle: blogtitle,
+    blogContent: blogContent,
   });
 };
